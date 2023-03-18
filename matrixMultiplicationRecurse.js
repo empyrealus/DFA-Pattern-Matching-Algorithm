33d7f0deb33f2139aa3e5609bd2a039ed2859cb0
Empyrealus <103290689+empyrealus@users.noreply.github.com>
2023-03-18 08:44:33 -0500

    Update matrixMultiplicationRecurse.js

--- matrixMultiplicationRecurse.js
+++ matrixMultiplicationRecurse.js
@@ -1,4 +1,4 @@
-// Theoretical Quantum Programing (w/o boolean)*
+// Theoretical Quantum Logic Programing (w/o boolean)*
 // Using recursion to perform matrix multiplication
 // features includes: valid matrix check (quantum logic gates), finite numbers only, terneary break statement work around with custom error reporting
 // Console.log(result) is used to show where the loop ends programmatically
